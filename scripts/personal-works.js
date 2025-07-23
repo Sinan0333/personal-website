bd05d4765def9be25ce94a73ae4632a3f5eda85d
sinan <sinan.m.p333@gmail.com>
2025-07-23 22:52:35 +0530

    Personal projects section added

---
+++ scripts/personal-works.js
@@ -0,0 +1,92 @@
+const personalWorks = [
+  {
+    img: "/img/works/bbc-i-player.png",
+    title: "BBC iPlayer Clone",
+    subtitle: "Streaming Platform Clone",
+    description:
+      "A clone of the BBC iPlayer streaming platform, featuring a user-friendly interface and dynamic content management.",
+    features: [
+      "Developed a clone of the BBC iPlayer streaming platform with a user-friendly interface.",
+      "Utilized React.js for front-end development, Tailwind CSS for styling and TMDB API for dynamic content management.",
+      "Implemented responsive design for optimal viewing on various devices.",
+    ],
+    tech: {
+      Frontend: ["React.js", "Tailwind CSS"],
+      API: ["TMDB API"],
+    },
+    link: "https://bbc-i-player.vercel.app/",
+  },
+  {
+    img: "/img/works/netflix-clone.png",
+    title: "Netflix Clone",
+    subtitle: "Streaming Platform Clone",
+    description:
+      "A clone of the Netflix streaming platform, featuring a user-friendly interface and dynamic content management.",
+    features: [
+      "Developed a clone of the Netflix streaming platform with a user-friendly interface.",
+      "Utilized React.js for front-end development, Tailwind CSS for styling and TMDB API for dynamic content management.",
+      "Implemented responsive design for optimal viewing on various devices.",
+      "Integrated a Trailers section to showcase movie trailers.",
+    ],
+    tech: {
+      Frontend: ["React.js", "Tailwind CSS"],
+      API: ["TMDB API"],
+    },
+    link: "https://netflix-clone-eosin-rho.vercel.app",
+  },
+  {
+    img: "/img/works/tic-tac-toe.png",
+    title: "Tic Tac Toe",
+    subtitle: "Tic Tac Toe Game",
+    description:
+      "A simple Tic Tac Toe game built with React.js, featuring a user-friendly interface and dynamic gameplay.",
+    features: [
+      "Developed a simple Tic Tac Toe game with React.js and Node.js.",
+      "Utilized Socket.IO for real-time multiplayer functionality.",
+      "Implemented a user-friendly interface for easy gameplay.",
+      "Features include player vs player mode",
+      "Responsive design for optimal viewing on various devices.",
+    ],
+    tech: {
+      Frontend: ["React.js", "Tailwind CSS"],
+      Backend: ["Node.js", "Express.js"],
+      RealTime: ["Socket.IO"],
+    },
+    link: "https://tic-tac-toe-client-dun.vercel.app",
+  },
+  {
+    img: "/img/works/zoofari.png",
+    title: "Zoofari",
+    subtitle: "Zoo Landing Page",
+    description:
+      "A zoo landing page built with React.js, Tailwind CSS and GSAP for animations, featuring a dynamic gallery and interactive elements.",
+    features: [
+      "Developed a zoo landing page with React.js and Tailwind CSS.",
+      "Implemented GSAP for smooth scroll animations and transitions.",
+    ],
+    tech: {
+      Frontend: ["React.js", "Tailwind CSS", "GSAP"],
+    },
+    link: "https://zoofari.vercel.app/",
+  },
+  {
+    img: "/img/works/space-x-clone.png",
+  },
+  {
+    img: "/img/works/olx-clone.png",
+  },
+  {
+    img: "/img/works/bose-clone.png",
+  },
+  {
+    img: "/img/works/lays-clone.png",
+  },
+    {
+    img: "/img/works/iphone-anim.png",
+  },
+];
+
+// Example links for personal works
+// https://sinan0333.github.io/Bose
+// https://sinan0333.github.io/Homelays
+// https://sinan0333.github.io/spaceX.sinan
--- scripts/portfolio-item.js
+++ scripts/portfolio-item.js
@@ -1,5 +1,4 @@
-const portfolioContainer = document.querySelector(".portfolios");
-
+const portfolioContainer = document.getElementById("portfolios");
 works.forEach((item, index) => {
   const portfolioHTML = `
         <div class="portfolio-item" data-index="${index}">
@@ -12,4 +11,19 @@ works.forEach((item, index) => {
         </div>
     `;
   portfolioContainer.innerHTML += portfolioHTML;
+});
+
+const personalWorksContainer = document.getElementById("personal-works");
+personalWorks.forEach((item, index) => {
+  const portfolioHTML = `
+        <div class="portfolio-item" data-index="${index}">
+            <div class="image">
+                <img src="${item.img}" alt="">
+            </div>
+            <div class="hover-items">
+                <h3>Tap to open</h3>
+            </div>
+        </div>
+    `;
+  personalWorksContainer.innerHTML += portfolioHTML;
 });
